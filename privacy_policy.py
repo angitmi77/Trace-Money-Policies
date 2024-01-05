218dbc7928ff6f18c54bf0ddc276a1b0532dba04
angitmi77 <145848655+angitmi77@users.noreply.github.com>
2024-01-05 19:34:04 +0000

    Update privacy_policy.py

--- privacy_policy.py
+++ privacy_policy.py
@@ -1,64 +1,9 @@
 import streamlit
 
-
 def privacy_policy_function():
         streamlit.subheader("Politiques de confidatialité de Trace Money")
         streamlit.write("---")
         streamlit.write("##")
-        streamlit.write("""
-
-Dernière mise à jour : 03 janvier 2024
-
-Cette politique de confidentialité décrit nos politiques et procédures relatives à la collecte, à l'utilisation et à la divulgation de vos informations lorsque vous utilisez le service et vous informe de vos droits à la vie privée et de la manière dont la loi vous protège.
-
-Nous utilisons vos données personnelles pour fournir et améliorer le service. En utilisant le Service, vous acceptez la collecte et l'utilisation d'informations conformément à la présente politique de confidentialité. Cette politique de confidentialité a été créée avec l'aide du générateur gratuit de politique de confidentialité.
-
-Interprétation et définitions Interprétation
-
-Les mots dont la lettre initiale est en majuscule ont des significations définies dans les conditions suivantes. Les définitions suivantes auront la même signification qu’elles apparaissent au singulier ou au pluriel.
-
-Définitions
-
-Aux fins de cette politique de confidentialité :
-
-Compte désigne un compte unique créé pour vous permettre d'accéder à notre service ou à des parties de notre service.
-Société affiliée désigne une entité qui contrôle, est contrôlée par ou est sous contrôle commun avec un parti, où « contrôle » désigne la propriété de 50 % ou plus des actions, participations ou autres titres donnant droit à voter pour l'élection d'administrateurs ou d'autres autorités de gestion. .
-L'application fait référence à Trace Money, le logiciel fourni par la Société.
-La Société (appelée « la Société », « Nous », « Notre » ou « Notre » dans le présent Accord) fait référence à Trace Money.
-Le pays fait référence à : Burkina Faso
-Appareil désigne tout appareil pouvant accéder au Service tel qu'un ordinateur, un téléphone portable ou une tablette numérique.
-Les données personnelles sont toute information relative à une personne identifiée ou identifiable.
-Service fait référence à l’Application.
-Prestataire désigne toute personne physique ou morale qui traite les données pour le compte de la Société. Il fait référence à des sociétés tierces ou à des personnes employées par la Société pour faciliter le Service, pour fournir le Service au nom de la Société, pour exécuter des services liés au Service ou pour aider la Société à analyser la manière dont le Service est utilisé.
-Les Données d'Utilisation font référence aux données collectées automatiquement, soit générées par l'utilisation du Service, soit à partir de l'infrastructure du Service elle-même (par exemple, la durée d'une visite d'une page).
-
-Vous désignez la personne qui accède ou utilise le Service, ou la société ou toute autre entité juridique au nom de laquelle cette personne accède ou utilise le Service, selon le cas.
-
-Collecte et utilisation de vos données personnelles Types de données collectées Données personnelles
-
-Lors de l'utilisation de notre service, nous pouvons vous demander de nous fournir certaines informations personnelles identifiables qui peuvent être utilisées pour vous contacter ou vous identifier. Les informations personnelles identifiables peuvent inclure, sans s’y limiter :
-Adresse e-mail
-Prénom et nom
-Des données d'utilisation
-
-Des données d'utilisation
-
-Les données d'utilisation sont collectées automatiquement lors de l'utilisation du service. Les données d'utilisation peuvent inclure des informations telles que l'adresse de protocole Internet de votre appareil (par exemple, l'adresse IP), le type de navigateur, la version du navigateur, les pages de notre service que vous visitez, l'heure et la date de votre visite, le temps passé sur ces pages, l'appareil unique. identifiants et autres données de diagnostic. Lorsque vous accédez au Service via ou via un appareil mobile, nous pouvons collecter automatiquement certaines informations, notamment, mais sans s'y limiter, le type d'appareil mobile que vous utilisez, l'identifiant unique de votre appareil mobile, l'adresse IP de votre appareil mobile, votre système d'exploitation, le type de navigateur Internet mobile que vous utilisez, les identifiants uniques de l'appareil et d'autres données de diagnostic. Nous pouvons également collecter des informations que votre navigateur envoie chaque fois que vous visitez notre service ou lorsque vous accédez au service via ou via un appareil mobile.
-
-Utilisation de vos données personnelles
-
-La Société peut utiliser les Données Personnelles aux fins suivantes :
-Pour fournir et maintenir notre service, y compris pour surveiller l'utilisation de notre service.
-Pour gérer Votre Compte : pour gérer Votre inscription en tant qu'utilisateur du Service. Les données personnelles que vous fournissez peuvent vous donner accès à différentes fonctionnalités du service qui sont à votre disposition en tant qu'utilisateur enregistré.
-
-Pour l'exécution d'un contrat : l'élaboration, le respect et l'exécution du contrat d'achat des produits, articles ou services que vous avez achetés ou de tout autre contrat avec nous via le Service.
-
-Pour vous contacter : pour vous contacter par e-mail, appels téléphoniques, SMS ou autres formes de communication électronique équivalentes, telles que les notifications push d'une application mobile concernant les mises à jour ou les communications informatives liées aux fonctionnalités, produits ou services souscrits, y compris les mises à jour de sécurité, lorsque cela est nécessaire ou raisonnable pour leur mise en œuvre.
-
-Pour vous fournir des actualités, des offres spéciales et des informations générales sur d'autres biens, services et événements que nous proposons et similaires à ceux que vous avez déjà achetés ou sur lesquels vous avez demandé des renseignements, à moins que vous ayez choisi de ne pas recevoir de telles informations.
-
-Contact : tresortamini@gmail.com
-
-        """)
+        streamlit.write("[Continuer](https://www.iubenda.com/privacy-policy/51742190)")
 
 
